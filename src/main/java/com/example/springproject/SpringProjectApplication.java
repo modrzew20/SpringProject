3466f0a2144313051f9ea9dc10a72903e82ffe53
Marcin Muszyński 229967 <80945672+Muszeczka@users.noreply.github.com>
2022-05-10 23:18:05 +0200

    Added GoogleMapsService

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -1,13 +1,16 @@
 package com.example.springproject;
 
-import org.springframework.boot.SpringApplication;
+import com.example.springproject.service.GoogleMapsService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringProjectApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        //SpringApplication.run(SpringProjectApplication.class, args);
+        GoogleMapsService googleMapsService = new GoogleMapsService();
+        System.out.print(googleMapsService.getDistanceRequest("Bardowskiego, 19a, Pabianice", "Sempołowskiej, 6, Pabianice"));
+
     }
 
 }
---
+++ src/main/java/com/example/springproject/service/GoogleMapsService.java
@@ -0,0 +1,35 @@
+package com.example.springproject.service;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+
+public class GoogleMapsService {
+    private final String my_key = "AIzaSyCYWcIJBZq1eS8sNcAACBkO0GJOXK8-5ig";
+
+    private static String GET_URL = "https://maps.googleapis.com/maps/api/directions/json?";
+
+    public GoogleMapsService() {
+    }
+
+    public int getDistanceRequest(String origin, String destination) throws IOException {
+
+        GET_URL = GET_URL + "origin=" + origin + "&destination=" + destination + "&key=" + my_key;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(GET_URL)
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        JSONObject jsonObject = new JSONObject(response.body().string());
+
+        return jsonObject.getJSONArray("routes").getJSONObject(0).getJSONArray("legs").optJSONObject(0).optJSONObject("distance").getInt("value");
+    }
+
+}
