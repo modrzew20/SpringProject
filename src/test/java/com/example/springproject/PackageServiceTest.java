6493ac6b389547ee64ec40373f775f117ed0baf4
230026 Topolska Kamila <230026@edu.p.lodz.pl>
2022-06-01 21:12:12 +0200

    Add PackageServiceTest

--- src/test/java/com/example/springproject/PackageServiceTest.java
+++ src/test/java/com/example/springproject/PackageServiceTest.java
@@ -1,76 +1,55 @@
-//package com.example.springproject;
-//
-//import com.example.springproject.model.Courier;
-//import com.example.springproject.model.Region;
-//import com.example.springproject.repository.CourierRepo;
-//import com.example.springproject.repository.PackageRepo;
-//import com.example.springproject.repository.RegionRepo;
-//import com.example.springproject.repository.repositoryExceptions.CantCreateRegion;
-//import com.example.springproject.repository.repositoryExceptions.PackageNotFound;
-//import com.example.springproject.repository.repositoryExceptions.RegionNotFound;
-//import com.example.springproject.service.PackageService;
-//import com.example.springproject.service.RegionService;
-//import org.junit.jupiter.api.*;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import com.example.springproject.model.Package;
-//
-//import java.util.UUID;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//public class PackageServiceTest {
-//
-//    private Package aPackage;
-//    private Courier aCourier;
-//
-//    private UUID findPackageUUID;
-//    private UUID courierUUID;
-//
-//    @Autowired
-//    private CourierRepo courierRepo;
-//
-//    @Autowired
-//    private PackageRepo packageRepo;
-//
-//    @Autowired
-//    private RegionRepo regionRepo;
-//
-//    @Autowired
-//    private PackageService packageService;
-//
-//    @BeforeAll
-//    void setUpAll() {
-//        findPackageUUID = UUID.randomUUID();
-//        courierUUID = UUID.randomUUID();
-//        aCourier = new Courier(courierUUID, 32.4, 12.4,
-//                new Region(UUID.randomUUID(), "region", 43.4, 12.4, 43.5, 12.5));
-//        aPackage = new Package(findPackageUUID, 23.1, 40.0, aCourier);
-//        packageRepo.create(aPackage);
-//    }
-//
-//    @Order(1)
-//    @Test
-//    public void findPackageTest() throws PackageNotFound {
-//        assertEquals(aPackage.getUuid(), packageService.findPackage(findPackageUUID).getUuid());
-//        assertThrows(PackageNotFound.class, () -> {
-//            packageService.findPackage(null);
-//        });
-//    }
-//
-//    @Order(2)
-//    @Test
-//    public void createPackageTest() {
-//
-//        assertEquals(packageService.getPackagesAssignToCourier(courierUUID).get(0).getUuid();
-//    }
-//
-//
-//
-//}
+package com.example.springproject;
+
+import com.example.springproject.repository.repositoryExceptions.ItemNotFoundException;
+import com.example.springproject.repository.repositoryExceptions.NoCourierForThisRegionException;
+import com.example.springproject.repository.repositoryExceptions.PackageNotFoundException;
+import com.example.springproject.service.PackageService;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PackageServiceTest {
+
+    @Autowired
+    private PackageService packageService;
+
+    @Order(1)
+    @Test
+    public void findPackageTest() throws PackageNotFoundException {
+        assertEquals(UUID.fromString("2a9dc82c-bfc1-47db-b037-3569d3949ef5"),
+                packageService.findPackage(UUID.fromString("2a9dc82c-bfc1-47db-b037-3569d3949ef5")).getUuid());
+        assertThrows(PackageNotFoundException.class, () -> {
+            packageService.findPackage(null);
+        });
+    }
+
+    @Order(2)
+    @Test
+    public void getPackagesAssignToCourierTest() {
+        assertEquals(UUID.fromString("2a9dc82c-bfc1-47db-b037-3569d3949ef5"),
+                packageService.getPackagesAssignToCourier(UUID.fromString("88eef4a2-3ca5-4bcf-b569-6f2ec20f483c")).get(0).getUuid());
+    }
+
+    @Order(3)
+    @Test
+    public void deletePackageTest() throws PackageNotFoundException {
+        assertTrue(packageService.deletePackage(UUID.fromString("2a9dc82c-bfc1-47db-b037-3569d3949ef5")));
+    }
+
+    @Order(4)
+    @Test
+    public void createPackageTest() throws NoCourierForThisRegionException, ItemNotFoundException {
+        assertTrue(packageService.createPackage(2,1));
+    }
+}
