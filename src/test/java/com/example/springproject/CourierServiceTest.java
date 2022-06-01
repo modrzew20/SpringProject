b9f647ddd06e3291f835392eee50683c67b97161
230026 Topolska Kamila <230026@edu.p.lodz.pl>
2022-06-01 20:29:19 +0200

    Update CourierServiceTest

--- src/test/java/com/example/springproject/CourierServiceTest.java
+++ src/test/java/com/example/springproject/CourierServiceTest.java
@@ -1,100 +1,67 @@
-//package com.example.springproject;
-//
-//import com.example.springproject.model.Courier;
-//import com.example.springproject.model.Region;
-//import com.example.springproject.repository.CourierRepo;
-//import com.example.springproject.repository.RegionRepo;
-//import com.example.springproject.repository.repositoryExceptions.*;
-//import com.example.springproject.service.CourierService;
-//import org.junit.jupiter.api.*;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.UUID;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//public class CourierServiceTest {
-//
-//    private UUID findCourierUUID;
-//    private UUID addRegionUUID;
-//    private UUID addNewRegionUUID;
-//    private UUID getCourierForRegionUUID;
-//
-//    private Courier courier;
-//
-//    @Autowired
-//    private RegionRepo regionRepo;
-//
-//    @Autowired
-//    private CourierRepo courierRepo;
-//
-//    @Autowired
-//    private CourierService courierService;
-//
-//    @BeforeAll
-//    void setUp() throws CantCreateCourier, CantCreateRegion, RegionNotFound {
-//        findCourierUUID = UUID.randomUUID();
-//        addRegionUUID = UUID.randomUUID();
-//        addNewRegionUUID = UUID.randomUUID();
-//        getCourierForRegionUUID = UUID.randomUUID();
-//
-//        regionRepo.create(new Region(addRegionUUID, "region_1",
-//                12.4, 123.4, 12.2, 12.3));
-//        regionRepo.create(new Region(addNewRegionUUID, "region_2",
-//                124.4, 1213.4, 412.2, 312.3));
-//        regionRepo.create(new Region(getCourierForRegionUUID, "region_2", 21.0,
-//                21.4, 32.1, 432.4));
-//
-//        courier = new Courier(findCourierUUID, 40.0,
-//                39.0, regionRepo.find(addRegionUUID));
-//        courierRepo.create(courier);
-//    }
-//
-//    @Order(1)
-//    @Test
-//    public void findCourierTest() throws CourierNotFound {
-//        assertEquals(courier.getUuid(), courierService.findCourier(findCourierUUID).getUuid());
-//        assertThrows(CourierNotFound.class, () -> {
-//            courierService.findCourier(null);
-//        });
-//    }
-//
-//    @Order(2)
-//    @Test
-//    public void createCourierTest() throws CantCreateRegion {
-//        assertThrows(RegionNotFound.class, () -> {
-//            courierService.createCourier(43.1, 43.2, UUID.randomUUID());
-//        });
-//        assertThrows(CantCreateCourier.class, () -> {
-//            courierService.createCourier(43.1, 43.3, addRegionUUID);
-//        });
-//
-//
-//        assertDoesNotThrow(() -> {
-//            courierService.createCourier(34.2, 30.0, addNewRegionUUID);
-//        });
-//    }
-//
-//    @Order(3)
-//    @Test
-//    public void getAssignCourierForRegionTest() throws RegionNotFound, NoCourierForThisRegion {
-//        assertThrows(NoCourierForThisRegion.class, () -> {
-//            courierService.getAssignCourierForRegion(getCourierForRegionUUID);
-//        });
-//        assertEquals(findCourierUUID, courierService.getAssignCourierForRegion(addRegionUUID).getUuid());
-//    }
-//
-//    @Order(4)
-//    @Test
-//    public void deleteCourierTest() throws CourierNotFound {
-//        assertTrue(courierService.deleteCourier(findCourierUUID));
-//    }
-//}
+package com.example.springproject;
+
+import com.example.springproject.model.Courier;
+import com.example.springproject.model.Region;
+import com.example.springproject.repository.CourierRepo;
+import com.example.springproject.repository.RegionRepo;
+import com.example.springproject.repository.repositoryExceptions.*;
+import com.example.springproject.service.CourierService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CourierServiceTest {
+
+    @Autowired
+    private CourierService courierService;
+
+    @Order(1)
+    @Test
+    public void findCourierTest() throws CourierNotFoundException {
+        assertEquals(UUID.fromString("88eef4a2-3ca5-4bcf-b569-6f2ec20f483c"), courierService.findCourier
+                (UUID.fromString("88eef4a2-3ca5-4bcf-b569-6f2ec20f483c")).getUuid());
+        assertThrows(CourierNotFoundException.class, () -> {
+            courierService.findCourier(null);
+        });
+    }
+
+    @Order(2)
+    @Test
+    public void createCourierTest() throws CantCreateCourierException {
+        assertDoesNotThrow(() -> {
+            courierService.createCourier(34.2, 30.0,
+                    UUID.fromString("b6d7bb81-732a-490e-bdf3-d3993bfe882b"));
+        });
+        assertThrows(CantCreateCourierException.class, () -> {
+            courierService.createCourier(34.2, 30.0,
+                    UUID.fromString("b6d7bb81-732a-490e-bdf3-d3993bfe882b"));
+        });
+    }
+
+    @Order(3)
+    @Test
+    public void getAssignCourierForRegionTest() throws RegionNotFoundException, NoCourierForThisRegionException {
+        assertThrows(NoCourierForThisRegionException.class, () -> {
+            courierService.getAssignCourierForRegion(UUID.fromString("df1941d5-20b3-48ae-9186-cd37b47aac37"));
+        });
+        assertEquals(UUID.fromString("88eef4a2-3ca5-4bcf-b569-6f2ec20f483c"),
+                courierService.getAssignCourierForRegion(UUID.fromString("26699969-59b4-42f1-8c70-6d403a522767")).getUuid());
+    }
+
+    @Order(4)
+    @Test
+    public void deleteCourierTest() throws CourierNotFoundException {
+        assertTrue(courierService.deleteCourier(UUID.fromString("1b12b987-b96a-4c17-9428-9376ff8e26e5")));
+    }
+}
