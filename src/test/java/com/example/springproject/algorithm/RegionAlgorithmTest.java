1de8fb01702e7ce866676d5ed259d90fd3fa34a6
JLewandowski0 <229939@edu.p.lodz.pl>
2022-06-07 21:31:26 +0200

    Tests done

--- src/test/java/com/example/springproject/algorithm/RegionAlgorithmTest.java
+++ src/test/java/com/example/springproject/algorithm/RegionAlgorithmTest.java
@@ -3,6 +3,7 @@ package com.example.springproject.algorithm;
 import com.example.springproject.repository.repositoryExceptions.CantCreateRegionException;
 import com.example.springproject.repository.repositoryExceptions.InvalidDataException;
 import com.example.springproject.service.RegionService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -29,6 +30,7 @@ public class RegionAlgorithmTest {
 
     @Test
     public void createRegionIncludingFirstRegionTest() {
+        // zawiera w sobie cały pierwszy region
         assertThrows(CantCreateRegionException.class, () -> {
             regionService.createRegion("second",30.0,5.0,5.0,25.0);
         });
@@ -36,17 +38,51 @@ public class RegionAlgorithmTest {
 
     @Test
     public void createOverlappingRegionTest() {
+        // pokrywa część pierwszego regionu
         assertThrows(CantCreateRegionException.class, () -> {
-            regionService.createRegion("name2",30.0,15.0,5.0,30.0);
+            regionService.createRegion("name2",30.0,15.0,15.0,30.0);
+        });
+        // pokrywa się jedna ściana regionu
+        assertDoesNotThrow(() -> {
+            regionService.createRegion("name2",20.0,10.0,20.0,30.0);
+        });
+    }
+
+    @Test
+    public void createRegionInsideFirstRegionTest() {
+        // region który zawiera się w pierwszym regionie
+        assertThrows(CantCreateRegionException.class, () -> {
+            regionService.createRegion("second",15.0,12.0,12.0,15.0);
+        });
+    }
+
+    @Test
+    public void createSameAreaRegionTest() {
+        //region pokrywa ten sam obszar
+        assertThrows(CantCreateRegionException.class, () -> {
+            regionService.createRegion("second", 20.0, 10.0, 10.0, 20.0);
         });
     }
     @Test
-    public void createRegionTest() {
+    public void createSimilarRegionTest() {
+        //region pokrywają się 2 przyległe boki
         assertThrows(CantCreateRegionException.class, () -> {
-            regionService.createRegion("name2",30.0,25.0,40.0,50.0);
+            regionService.createRegion("second", 20.0, 15.0, 10.0, 15.0);
         });
-        assertDoesNotThrow( () -> {
-            regionService.createRegion("name2",30.0,25.0,40.0,45.0);
+    }
+
+    @Test
+    public void createRegionIncludingPartOfFirstRegionTest() {
+        //region pokrywa kawałek pierwszego regionu i sciany sie nie pokrywają
+        assertThrows(CantCreateRegionException.class, () -> {
+            regionService.createRegion("second", 19.0, 5.0, 11.0, 15.0);
+        });
+    }
+    @Test
+    public void createRegionIncludingThreeWallsOfFirstRegionTest() {
+        //
+        assertThrows(CantCreateRegionException.class, () -> {
+            regionService.createRegion("second", 20.0, 15.0, 10.0, 20.0);
         });
     }
 }
